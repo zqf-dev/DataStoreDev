38f433ee70df345e03fd27dbcdd976a11fe5857c
zhangqingfa <1583510917@qq.com>
2023-01-12 13:55:36 +0800

    封装

--- app/src/main/java/com/zqf/datastoredev/MainActivity.kt
+++ app/src/main/java/com/zqf/datastoredev/MainActivity.kt
@@ -3,20 +3,27 @@ package com.zqf.datastoredev
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.zqf.datastoredev.util.editor
+import com.zqf.datastoredev.util.putString
 import com.zqf.datastoredev.util.sp
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initUtil()
+        initSpUtil()
+        initFiles()
     }
 
-    private fun initUtil() {
-        editor.putString("string_key", "123")
-        editor.commit()
-        val spStrValue = sp.getString("string_key", "")
-        Log.e("TAG", "spV: $spStrValue")
+    private fun initFiles() {
+        val fos: FileOutputStream = openFileOutput("testFile.txt", MODE_PRIVATE)
+        fos.write("123".toByteArray())
+    }
+
+    private fun initSpUtil() {
+        putString("string_key", "123")
+        val spStrV = sp.getString("string_key", "")
+        Log.e("TAG", "spV: $spStrV")
     }
 }
--- app/src/main/java/com/zqf/datastoredev/util/SPUtil.kt
+++ app/src/main/java/com/zqf/datastoredev/util/SPUtil.kt
@@ -4,6 +4,19 @@ import android.content.Context
 import android.content.SharedPreferences
 import com.zqf.datastoredev.App
 
-val sp: SharedPreferences = App.context.getSharedPreferences("", Context.MODE_PRIVATE)
+val pkName: String = App.context.packageName
+
+val sp: SharedPreferences = App.context.getSharedPreferences(pkName, Context.MODE_PRIVATE)
 
 val editor: SharedPreferences.Editor = sp.edit()
+
+fun putString(key: String, value: String) {
+    editor.putString(key, value)
+    editor.commit()
+}
+
+fun putInt(key: String, value: Int) {
+    editor.putInt(key, value)
+    editor.commit()
+}
+
