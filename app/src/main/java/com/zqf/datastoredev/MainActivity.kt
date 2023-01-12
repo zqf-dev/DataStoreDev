7d91014a9398805f0f9c05b592480f5f00845257
zhangqingfa <1583510917@qq.com>
2023-01-12 09:32:31 +0800

    调整文件

--- app/src/main/java/com/zqf/datastoredev/MainActivity.kt
+++ app/src/main/java/com/zqf/datastoredev/MainActivity.kt
@@ -7,6 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val edit = sp.edit()
+        initUtil()
+    }
+
+    private fun initUtil() {
+
     }
 }
