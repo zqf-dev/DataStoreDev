9831f95d2141c09579c6d344cf90d37969afcff8
zhangqingfa <1583510917@qq.com>
2023-01-16 14:08:37 +0800

    调整存储

--- app/src/main/java/com/zqf/datastoredev/util/SPUtil.kt
+++ app/src/main/java/com/zqf/datastoredev/util/SPUtil.kt
@@ -2,6 +2,7 @@ package com.zqf.datastoredev.util
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.util.Log
 import com.zqf.datastoredev.App
 
 val pkName: String = App.context.packageName
@@ -11,12 +12,31 @@ val sp: SharedPreferences = App.context.getSharedPreferences(pkName, Context.MOD
 val editor: SharedPreferences.Editor = sp.edit()
 
 fun putString(key: String, value: String) {
-    editor.putString(key, value)
-    editor.commit()
+    editor.apply {
+        putString(key, value)
+        commit()
+    }
 }
 
 fun putInt(key: String, value: Int) {
-    editor.putInt(key, value)
+    editor.apply {
+        putInt(key, value)
+        commit()
+    }
+}
+
+fun putLong(key: String, value: Long) {
+    editor.putLong(key, value)
+    editor.commit()
+}
+
+fun putBoolean(key: String, value: Boolean) {
+    editor.putBoolean(key, value)
+    editor.commit()
+}
+
+fun putFloat(key: String, value: Float) {
+    editor.putFloat(key, value)
     editor.commit()
 }
 
